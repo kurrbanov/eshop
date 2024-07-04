f014dd4ed8e8f88bc4f8e26ab6b099dd699b8da8
kurrbanov <justnick1312@gmail.com>
2024-07-04 21:39:12 +0300

    feat: fbv

--- server/urls.py
+++ server/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from shop.views import main_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_page),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+from shop.models import Product
+
+
+def main_page(request: HttpRequest):
+    print(f"{request.method=}")
+    print(f"{request.content_type=}")
+    print(f"{request.GET}")
+
+    product_titles = Product.objects.values_list("title", flat=True)
+    titles = b""
+    for title in product_titles:
+        titles += title.encode("utf-8") + b"\n"
+    print(f"{titles.decode('utf-8')=}")
+    return HttpResponse(
+        b"<!DOCTYPE html>"
+        b"<html>"
+        b"<body>" + titles +
+        b"</body>"
+        b"</html>"
+    )
