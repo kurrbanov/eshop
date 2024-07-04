e79343df62bbfe570735015e00f100cb69c0b9cd
kurrbanov <justnick1312@gmail.com>
2024-07-04 22:21:45 +0300

    feat: add dtl

--- shop/views.py
+++ shop/views.py
@@ -5,19 +5,9 @@ from shop.models import Product
 
 
 def main_page(request: HttpRequest):
-    print(f"{request.method=}")
-    print(f"{request.content_type=}")
-    print(f"{request.GET}")
-
-    product_titles = Product.objects.values_list("title", flat=True)
-    titles = b""
-    for title in product_titles:
-        titles += title.encode("utf-8") + b"\n"
-    print(f"{titles.decode('utf-8')=}")
-    return HttpResponse(
-        b"<!DOCTYPE html>"
-        b"<html>"
-        b"<body>" + titles +
-        b"</body>"
-        b"</html>"
+    products = Product.objects.all()
+    return render(
+        request,
+        "index.html",
+        context={"products": products}
     )
